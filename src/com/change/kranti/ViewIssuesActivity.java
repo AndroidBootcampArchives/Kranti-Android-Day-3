068cb482af2434bed0ea8c00a8c29547f06dfb2c
NazneenRupawalla <nazneenr@thoughtworks.com>
2013-05-09 16:18:22 +0530

    Final Commit

--- src/com/change/kranti/ViewIssuesActivity.java
+++ src/com/change/kranti/ViewIssuesActivity.java
@@ -1,17 +1,13 @@
 package com.change.kranti;
 
-import android.app.Activity;
+import adapters.ViewIssuesAdapter;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import model.Issue;
+import android.widget.*;
 import repository.IssueRepository;
 
-import java.util.List;
-
-public class ViewIssuesActivity extends Activity {
+public class ViewIssuesActivity extends ListActivity {
 
     private IssueRepository issueRepository;
 
@@ -20,19 +16,15 @@ public class ViewIssuesActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.issues);
         issueRepository = new IssueRepository(getApplicationContext());
-        LinearLayout layout = (LinearLayout) findViewById(R.id.listofitems);
-        List<Issue> issues  = issueRepository.getIssues();
-        LayoutInflater layoutInflater = getLayoutInflater();
-        for (Issue issue : issues) {
-            View view = layoutInflater.inflate(R.layout.issue_item, null);
-            TextView titleView = (TextView)view.findViewById(R.id.title);
-            titleView.setText(issue.getTitle());
-            TextView descriptionView = (TextView)view.findViewById(R.id.description);
-            descriptionView.setText(issue.getDescription());
-            layout.addView(view);
-        }
+        ViewIssuesAdapter adapter =new ViewIssuesAdapter(this,R.id.issue_item,R.id.title,issueRepository.getIssues());
+        setListAdapter(adapter);
 
     }
 
-
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        TextView viewById =(TextView) v.findViewById(R.id.title);
+        Toast.makeText(getApplicationContext(),viewById.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
 }
